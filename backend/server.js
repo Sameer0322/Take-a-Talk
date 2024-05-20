a458a4ae34a282274eb1e04ec196357fedb194ee
Sameer0322 <116619265+Sameer0322@users.noreply.github.com>
2024-05-20 11:07:33 +0530

    Updated Server.js

--- backend/server.js
+++ backend/server.js
@@ -1,81 +1,60 @@
-const express = require("express")
-const dotenv = require("dotenv")
-const { chats } = require("./data/data")
-const connectDB = require("./config/db")
-const colors = require("colors")
-const userRoutes = require("./routes/userRoutes")
-const {notFound,errorHandler} = require("./middleware/errorMiddleware")
-const chatRoutes = require("./routes/chatRoutes")
-const messageRoutes = require("./routes/messageRoutes")
-const path = require("path")
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
+const userRoutes = require("./routes/userRoutes");
+const chatRoutes = require("./routes/chatRoutes");
+const messageRoutes = require("./routes/messageRoutes");
+const { notFound, errorHandler } = require("./middleware/errorMiddleware");
+const path = require("path");
 
-dotenv.config()
+dotenv.config();
+connectDB();
+const app = express();
 
-connectDB()
-const app = express()
+app.use(express.json()); // to accept json data
 
-app.use(express.json())
+// app.get("/", (req, res) => {
+//   res.send("API Running!");
+// });
+
+app.use("/api/user", userRoutes);
+app.use("/api/chat", chatRoutes);
+app.use("/api/message", messageRoutes);
 
 
+// Error Handling middlewares
+app.use(notFound);
+app.use(errorHandler);
 
-app.use('/api/user', userRoutes)
-app.use('/api/chat',chatRoutes)
-app.use('/api/message',messageRoutes)
+const PORT = process.env.PORT;
 
-//-------------Deployement----------------------------------------------------------------------------
+const server = app.listen(
+  PORT,
+  console.log(`Server running on PORT ${PORT}...`.yellow.bold)
+);
 
-const __dirname1 = path.resolve();
+const io = require("socket.io")(server, {
+  pingTimeout: 60000,
+  cors: {
+    origin: "*",
+  },
+});
 
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
-} else {
-  app.get("/", (req, res) => {
-    res.send("API is running..");
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
   });
-}
 
-//-------------Deployement----------------------------------------------------------------------------
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User Joined Room: " + room);
+  });
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
 
-
-app.use(notFound)
-app.use(errorHandler)
-
-
-const PORT = process.env.PORT || 5000
-
-const server = app.listen(PORT,console.log(`Server Started at PORT ${PORT}`.yellow.bold))
-
-
-const io = require('socket.io')(server, {
-    pingTimeout: 60000,
-    cors:{
-        origin:"*"
-    }
-})
-
-io.on("connection", (socket)=>{
-    console.log("connected to socket.io");
-
-    socket.on('setup', (userData)=>{
-        socket.join(userData._id);
-        // console.log(userData._id);
-        socket.emit("connected");
-    })
-
-
-    socket.on('join chat', (room) => {
-        socket.join(room);
-        console.log("User Joined room: "+ room);
-    });
-
-    socket.on("typing", (room) => socket.in(room).emit("typing"));
-    socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
-
-    socket.on("new message", (newMessageRecieved) => {
+  socket.on("new message", (newMessageRecieved) => {
     var chat = newMessageRecieved.chat;
 
     if (!chat.users) return console.log("chat.users not defined");
@@ -92,5 +71,24 @@ io.on("connection", (socket)=>{
     console.log("USER DISCONNECTED");
     socket.leave(userData._id);
   });
-  
+
 });
+
+
+// --------------------------deployment------------------------------
+
+const __dirname1 = path.resolve();
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
+}
+
+// --------------------------deployment------------------------------
